ecb9b255b1843a3aab5d9eb1b93b8ab4775bece4
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-10-07 20:39:17 -0500

    able to get backend server up and running - connected to MongoDB

---
+++ backend/server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("Successfully connected to MongoDB"))
+.catch(err => console.error("Error connecting to MongoDB:", err));
+
+app.get("/", (req, res) => {
+    res.json({message: 'Welcome to the CAPSTINE!!!'})
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
