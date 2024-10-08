6adbc8c56a1420f11feddb77e6de63d492fe8de0
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-10-08 15:26:06 -0500

    added basic app.jsx structure for rendering content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import Home from './components/Home.jsx';
+import Game from './components/Game.jsx';
+// import LeaderBoard from './components/LeaderBoard.jsx';
+import Profile from './components/Profile.jsx';
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/game' element={<Game />} />
+        {/* <Route path='/leaderboard' element={<LeaderBoard />} /> */}
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </Router>
   );
 };
 
