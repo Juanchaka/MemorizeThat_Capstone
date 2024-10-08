6a2ef3bef22aac72100183146b53d852e586f670
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-10-08 10:36:00 -0500

    userSchema created

--- backend/server/models/userSchema.js
+++ backend/server/models/userSchema.js
@@ -2,8 +2,43 @@ import mongoose from 'mongoose';
 import bycrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
-    username: {},
-    email: {},
-    password: {},
-    createdAt: {}
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
+
+userSchema.index({ usaername: 1, email: 1});
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+    this.password = await bycrypt.hash(this.password, 12);
+    next();
+});
+
+userSchema.methods.isValidPassword = async function(password) {
+    return await bycrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
