7bb965374b6899499c7d76ba2fdd8e1cc09cc627
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-10-12 14:17:00 -0500

    added more photos to the game -finishing MVP

--- frontend/src/utils/gameUtils.jsx
+++ frontend/src/utils/gameUtils.jsx
@@ -1,34 +1,10 @@
-// export const generateCards = (pairCount) => {
-//   pairCount = Math.floor(pairCount / 2) * 2;
-//   if (pairCount * 2 % 4 !== 0) {
-//     pairCount += 2;
-//   }
-
-//   const values = Array.from({ length: pairCount }, (_, i) => i + 1);
-//   const pairs = [...values, ...values];
-//   return pairs.map((value, index) => ({
-//     id: index,
-//     value: value,
-//   }));
-// };
-
-// export const shuffleCards = (cards) => {
-//   const shuffled = [...cards];
-//   for (let i = shuffled.length - 1; i > 0; i--) {
-//     const j = Math.floor(Math.random() * (i + 1));
-//     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-//   }
-//   return shuffled;
-// };
-
-
 export const generateCards = (pairCount) => {
   pairCount = Math.floor(pairCount / 2) * 2;
   if (pairCount * 2 % 4 !== 0) {
     pairCount += 2;
   }
 
-  const totalImages = 20;
+  const totalImages = 30;
   const selectedIndices = [];
 
   while (selectedIndices.length < pairCount) {
@@ -55,3 +31,27 @@ export const shuffleCards = (cards) => {
   }
   return shuffled;
 };
+
+//Initial game in case the photos fail
+// export const generateCards = (pairCount) => {
+//   pairCount = Math.floor(pairCount / 2) * 2;
+//   if (pairCount * 2 % 4 !== 0) {
+//     pairCount += 2;
+//   }
+
+//   const values = Array.from({ length: pairCount }, (_, i) => i + 1);
+//   const pairs = [...values, ...values];
+//   return pairs.map((value, index) => ({
+//     id: index,
+//     value: value,
+//   }));
+// };
+
+// export const shuffleCards = (cards) => {
+//   const shuffled = [...cards];
+//   for (let i = shuffled.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+//   }
+//   return shuffled;
+// };
