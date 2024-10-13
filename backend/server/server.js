c390cbedbdafebe78ec2984fb6f46abffb469d2b
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-10-13 10:44:18 -0500

    edited server-side code to remove dup and only serve server not frontend

--- backend/server/server.js
+++ backend/server/server.js
@@ -3,16 +3,12 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import apiRoutes from './routes/apiRoutes.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 dotenv.config();
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const corsOptions = {
-    origin: 'https://juanchaka.github.io/MemorizeThat_Capstone/',
+    origin: ['https://juanchaka.github.io', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -27,12 +23,6 @@ mongoose.connect(process.env.MONGODB_URI)
 .catch(err => console.error("Error connecting to MongoDB:", err));
 
 app.use('/api', apiRoutes);
-
-app.use(express.static(path.join(__dirname, '../../frontend/dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
-  });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
