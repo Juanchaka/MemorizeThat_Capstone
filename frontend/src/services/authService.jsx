556d3917dc05fe707792c3f62a65b075e9e9320c
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-10-13 09:42:34 -0500

    updated URLS and proxy config

--- frontend/src/services/authService.jsx
+++ frontend/src/services/authService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.VITE_APP_API_URL || '/api';
 
 export const loginUser = async (email, password) => {
   try {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/MemorizeThat_Capstone/",
@@ -12,7 +11,8 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://memorize-that.onrender.com',
-        changeOrigin: true
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
