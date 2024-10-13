a8b3f357ea52524308f4caea30e166bcd3f8c5f9
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-10-12 22:31:49 -0500

    removed commented out code

--- frontend/src/utils/gameUtils.jsx
+++ frontend/src/utils/gameUtils.jsx
@@ -31,27 +31,3 @@ export const shuffleCards = (cards) => {
   }
   return shuffled;
 };
-
-//Initial game in case the photos fail
-// export const generateCards = (pairCount) => {
-//   pairCount = Math.floor(pairCount / 2) * 2;
-//   if (pairCount * 2 % 4 !== 0) {
-//     pairCount += 2;
-//   }
-
-//   const values = Array.from({ length: pairCount }, (_, i) => i + 1);
-//   const pairs = [...values, ...values];
-//   return pairs.map((value, index) => ({
-//     id: index,
-//     value: value,
-//   }));
-// };
-
-// export const shuffleCards = (cards) => {
-//   const shuffled = [...cards];
-//   for (let i = shuffled.length - 1; i > 0; i--) {
-//     const j = Math.floor(Math.random() * (i + 1));
-//     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-//   }
-//   return shuffled;
-// };
