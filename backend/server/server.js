b8a98362e5ee5416c3e627f24c4086f010d54135
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-10-13 05:52:46 -0500

    Updates actions

--- backend/server/server.js
+++ backend/server/server.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const corsOptions = {
-    origin: '*',
+    origin: 'https://juanchaka.github.io/MemorizeThat_Capstone/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
