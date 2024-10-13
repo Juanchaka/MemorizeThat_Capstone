9059779e28d0f8fc49c5607f8ad5c7d6e6891f0a
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-10-13 10:51:57 -0500

    edited server-side files for redeployment

--- backend/server/server.js
+++ backend/server/server.js
@@ -24,6 +24,10 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.use('/api', apiRoutes);
 
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the MemorizeThat API' });
+  });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
